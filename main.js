2e8fc0845079f9a5fdc2334362d42f76afaf42bb
Tristan Huckabee <61269588+tristan-huckabee@users.noreply.github.com>
2021-05-05 23:02:47 -0500

    Uploaded HTML, CSS, and Js

---
+++ main.js
@@ -0,0 +1,179 @@
+let one = document.getElementsByClassName("item")[0];
+document.getElementsByClassName("fa-times-circle")[0].addEventListener("click", closeOne);
+one.addEventListener("click", popOne);
+function popOne(){
+    let obj = document.getElementsByClassName("popUp")[0];
+    
+    if(obj.style.visibility == "hidden"){
+        obj.style.visibility = "visible";
+    }
+}
+function closeOne(){
+    let obj = document.getElementsByClassName("popUp")[0];
+    
+    if(obj.style.visibility == "visible"){
+        obj.style.visibility = "hidden";
+    }
+}
+
+let two = document.getElementsByClassName("item")[1];
+document.getElementsByClassName("fa-times-circle")[1].addEventListener("click", closeTwo);
+two.addEventListener("click", popTwo);
+function popTwo(){
+    let obj = document.getElementsByClassName("popUp")[1];
+    
+    if(obj.style.visibility == "hidden"){
+        obj.style.visibility = "visible";
+    }
+}
+function closeTwo(){
+    let obj = document.getElementsByClassName("popUp")[1];
+    
+    if(obj.style.visibility == "visible"){
+        obj.style.visibility = "hidden";
+    }
+}
+
+let three = document.getElementsByClassName("item")[2];
+document.getElementsByClassName("fa-times-circle")[2].addEventListener("click", closethree);
+three.addEventListener("click", popthree);
+function popthree(){
+    let obj = document.getElementsByClassName("popUp")[2];
+    
+    if(obj.style.visibility == "hidden"){
+        obj.style.visibility = "visible";
+    }
+}
+function closethree(){
+    let obj = document.getElementsByClassName("popUp")[2];
+    
+    if(obj.style.visibility == "visible"){
+        obj.style.visibility = "hidden";
+    }
+}
+
+let four = document.getElementsByClassName("item")[3];
+document.getElementsByClassName("fa-times-circle")[3].addEventListener("click", closefour);
+four.addEventListener("click", popfour);
+function popfour(){
+    let obj = document.getElementsByClassName("popUp")[3];
+    
+    if(obj.style.visibility == "hidden"){
+        obj.style.visibility = "visible";
+    }
+}
+function closefour(){
+    let obj = document.getElementsByClassName("popUp")[3];
+    
+    if(obj.style.visibility == "visible"){
+        obj.style.visibility = "hidden";
+    }
+}
+
+let five = document.getElementsByClassName("item")[4];
+document.getElementsByClassName("fa-times-circle")[4].addEventListener("click", closefive);
+five.addEventListener("click", popfive);
+function popfive(){
+    let obj = document.getElementsByClassName("popUp")[4];
+    
+    if(obj.style.visibility == "hidden"){
+        obj.style.visibility = "visible";
+    }
+}
+function closefive(){
+    let obj = document.getElementsByClassName("popUp")[4];
+    
+    if(obj.style.visibility == "visible"){
+        obj.style.visibility = "hidden";
+    }
+}
+
+let six = document.getElementsByClassName("item")[5];
+document.getElementsByClassName("fa-times-circle")[5].addEventListener("click", closesix);
+six.addEventListener("click", popsix);
+function popsix(){
+    let obj = document.getElementsByClassName("popUp")[5];
+    
+    if(obj.style.visibility == "hidden"){
+        obj.style.visibility = "visible";
+    }
+}
+function closesix(){
+    let obj = document.getElementsByClassName("popUp")[5];
+    
+    if(obj.style.visibility == "visible"){
+        obj.style.visibility = "hidden";
+    }
+}
+
+let seven = document.getElementsByClassName("item")[6];
+document.getElementsByClassName("fa-times-circle")[6].addEventListener("click", closeseven);
+seven.addEventListener("click", popseven);
+function popseven(){
+    let obj = document.getElementsByClassName("popUp")[6];
+    
+    if(obj.style.visibility == "hidden"){
+        obj.style.visibility = "visible";
+    }
+}
+function closeseven(){
+    let obj = document.getElementsByClassName("popUp")[6];
+    
+    if(obj.style.visibility == "visible"){
+        obj.style.visibility = "hidden";
+    }
+}
+
+let eight = document.getElementsByClassName("item")[7];
+document.getElementsByClassName("fa-times-circle")[7].addEventListener("click", closeeight);
+eight.addEventListener("click", popOne);
+function popeight(){
+    let obj = document.getElementsByClassName("popUp")[7];
+    
+    if(obj.style.visibility == "hidden"){
+        obj.style.visibility = "visible";
+    }
+}
+function closeeight(){
+    let obj = document.getElementsByClassName("popUp")[7];
+    
+    if(obj.style.visibility == "visible"){
+        obj.style.visibility = "hidden";
+    }
+}
+
+let nine = document.getElementsByClassName("item")[8];
+document.getElementsByClassName("fa-times-circle")[8].addEventListener("click", closenine);
+nine.addEventListener("click", popnine);
+function popnine(){
+    let obj = document.getElementsByClassName("popUp")[8];
+    
+    if(obj.style.visibility == "hidden"){
+        obj.style.visibility = "visible";
+    }
+}
+function closenine(){
+    let obj = document.getElementsByClassName("popUp")[8];
+    
+    if(obj.style.visibility == "visible"){
+        obj.style.visibility = "hidden";
+    }
+}
+
+let zero = document.getElementsByClassName("lemon")[0];
+document.getElementsByClassName("fa-times-circle")[9].addEventListener("click", closezero);
+zero.addEventListener("click", popzero);
+function popzero(){
+    let obj = document.getElementsByClassName("popUp")[9];
+    
+    if(obj.style.visibility == "hidden"){
+        obj.style.visibility = "visible";
+    }
+}
+function closezero(){
+    let obj = document.getElementsByClassName("popUp")[9];
+    
+    if(obj.style.visibility == "visible"){
+        obj.style.visibility = "hidden";
+    }
+}
