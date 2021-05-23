bf050ff9b495fe6bb1e0fddfd0b46da607b508c2
Tristan Huckabee <61269588+tristan-huckabee@users.noreply.github.com>
2021-05-23 03:26:42 -0500

    Redesigned Website

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//Expanding Cards
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach( (panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClass();
+        panel.classList.add('active');
+    });
+});
+
+function removeActiveClass() {
+    panels.forEach( (panel) => {
+        panel.classList.remove('active');
+    });
+}
+
+console.clear();
